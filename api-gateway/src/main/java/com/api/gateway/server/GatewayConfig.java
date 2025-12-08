a31ab3ed68d42b10dae07cfd18184a6a464ead79
shakil <shakilpatel2396@gmail.com>
2025-12-08 13:52:47 +0530

    adding api-gateway in our projects

---
+++ api-gateway/src/main/java/com/api/gateway/server/GatewayConfig.java
@@ -0,0 +1,2 @@
+package com.api.gateway.server;public class GatewayConfig {
+}
--- quize-service/src/main/java/com/substring/quiz/service/CategoryFeignService.java
+++ quize-service/src/main/java/com/substring/quiz/service/CategoryFeignService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@FeignClient(name = "category-service", url = "http://localhost:9091")
+@FeignClient(name = "CATEGORY-SERVICE")
 public interface CategoryFeignService {
 
     // get all the categories
